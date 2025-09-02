5f23fbbdbd00c6497493eeb6cfa1eab46ca4f4c2
Spyros Stafylarakis <spyridon.stafylarakis@signaloid.com>
2025-09-02 11:17:42 +0300

    Adds multiple choice component
    Adds a new component that allows users to select a single option from a list of choices.
    
    This component is implemented using Angular Material radio buttons and supports disabling the component and setting a default value. It also implements the ControlValueAccessor interface, allowing it to be used with Angular forms.

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -24,3 +24,4 @@ export * from './input-otp/input-otp.component';
 export * from './password-strength/password-strength.component';
 export * from './code-editor/code-editor.component';
 export * from './tabs-task-output/tabs-task-output.component';
+export * from './multiple-choice/multiple-choice.component';
