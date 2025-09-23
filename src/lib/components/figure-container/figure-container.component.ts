07da14369b53430f023c6538b8406d4fb7aafc06
Spyros Stafylarakis <spyridon.stafylarakis@signaloid.com>
2025-09-23 20:33:52 +0300

    Improves figure container loading states.
    Adds loading skeletons and improves the loading state presentation
    for both single-value and multi-element figures. Adjusts figure
    container styles to better handle loading and content display.
    Addresses visual consistency during data loading.

--- src/lib/components/figure-container/figure-container.component.ts
+++ src/lib/components/figure-container/figure-container.component.ts
@@ -3,24 +3,24 @@ import {ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges} from '@an
 import { DistributionPlotComponent } from '../distribution-plot/distribution-plot.component';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import {NgClass} from '@angular/common';
+import { SkeletonModule } from 'primeng/skeleton';
+import { ProgressSpinner } from 'primeng/progressspinner';
 
 @Component({
 	selector: 'lib-figure-container',
 	templateUrl: './figure-container.component.html',
 	styleUrl: './figure-container.component.css',
-	imports: [DistributionPlotComponent, MatProgressSpinner, NgClass],
+	imports: [DistributionPlotComponent, MatProgressSpinner, NgClass, SkeletonModule, ProgressSpinner],
 })
-export class FigureContainerComponent{
+export class FigureContainerComponent {
 	@Input() imageSource: string = '';
 	@Input() uxString: string = '';
 	@Input() xAxisLabel: string = '';
 	@Input() loading: boolean = false;
 	@Input() suffix: string = '';
 	@Input() prefix: string = '';
-  @Input() percentageOfValueAtRisk: number | undefined;
-  @Input() singleValue = false;
-  constructor(private cd: ChangeDetectorRef) {
-  }
-
+	@Input() percentageOfValueAtRisk: number | undefined;
+	@Input() singleValue = false;
+	constructor(private cd: ChangeDetectorRef) {}
 }
 
