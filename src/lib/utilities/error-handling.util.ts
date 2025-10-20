e741008af08673bf5daa217d5bef460c6b005527
Spyros Stafylarakis <spyridon.stafylarakis@signaloid.com>
2025-10-20 10:53:08 +0300

    Improves error message handling
    Enhances error handling to provide more informative messages to the user.
    
    Adds a check for `error.error` to extract and display the underlying error message. Also includes more comprehensive error logging and warnings to aid in debugging unhandled errors.
    
    Fixes issue where some errors were not being handled gracefully, resulting in unhelpful messages.
    
    Fixes #105

--- src/lib/utilities/error-handling.util.ts
+++ src/lib/utilities/error-handling.util.ts
@@ -1,17 +1,21 @@
 export function makeErrorReadable(error: any) {
 
-  if(error?.message.includes('Failed to retrieve ID token')) {
+  if(error?.message?.includes('Failed to retrieve ID token')) {
     return 'Authorization failed. Please log in again.';
   }
 
   if(error?.status === 401) {
     return 'Authorization failed. Please log in again.';
   }
-  if (error?.message.includes('failure during parsing')) {
+  if (error?.message?.includes('failure during parsing')) {
     return 'Invalid task output. Please try again.';
   }
-
+  if(error?.error) {
+    console.log(error);
+    return error?.error?.message
+  }
   if (error?.message) {
+    console.error(error);
     console.warn('Error message is unhandled', error);
     return error.message;
   }
