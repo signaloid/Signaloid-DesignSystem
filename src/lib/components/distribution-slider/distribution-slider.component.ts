e63dbb8ae26ea134a6d8ca2548a9ea3f5d96e09e
tfotakis-signaloid <213429317+tfotakis-signaloid@users.noreply.github.com>
2026-03-20 17:48:35 +0200

    feat: modernize Angular templates

--- src/lib/components/distribution-slider/distribution-slider.component.ts
+++ src/lib/components/distribution-slider/distribution-slider.component.ts
@@ -11,7 +11,7 @@ import {
 	SimpleChanges,
 	ViewChild,
 } from '@angular/core';
-import { DecimalPipe, NgForOf, NgIf } from '@angular/common';
+import { DecimalPipe } from '@angular/common';
 import { InputTextComponent } from '../input-text/input-text.component';
 import { InputTextSize } from '../input-text/input-text.models';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -27,7 +27,7 @@ interface DistributionPoint {
 @Component({
 	selector: 'lib-distribution-slider',
 	templateUrl: './distribution-slider.component.html',
-	imports: [NgForOf, NgIf, DecimalPipe, InputTextComponent, ReactiveFormsModule, ButtonComponent, BigNumbersPipe],
+	imports: [DecimalPipe, InputTextComponent, ReactiveFormsModule, ButtonComponent, BigNumbersPipe],
 	standalone: true,
 	styleUrls: ['./distribution-slider.component.scss'],
 })
