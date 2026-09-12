ad6c44642ca9eb2458df017e2c51c577a7c51f40
tfotakis-signaloid <213429317+tfotakis-signaloid@users.noreply.github.com>
2026-09-12 13:35:59 +0300

    fix: minor variable assignment fix in `updateStaging.sh`

--- updateStaging.sh
+++ updateStaging.sh
@@ -12,7 +12,7 @@ else
 fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-CURRENT_BRANCH=git branch --show-current
+CURRENT_BRANCH=$(git branch --show-current)
 
 git checkout staging
 
